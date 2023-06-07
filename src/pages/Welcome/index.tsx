ea18d97cde7eb458b051a3f9d0d610e863a41ea3
Pedro Lima <pedro.oficial1969@gmail.com>
2023-06-07 10:44:11 -0300

    feat(funcionalidade): adiciona funcionalidade de Sair para página welcome.

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
 function Welcome() {
+  const navigate = useNavigate();
+
   return (
     <section className="bg-[#070709] rounded-[34px] h-[480px] w-[800px]">
       <div className="w-full h-full flex flex-col items-center justify-center">
@@ -12,7 +15,12 @@ function Welcome() {
 
         <BsFillCheckCircleFill className="text-[#070709] text-[100px] bg-gradient-to-t from-[#4158D0] via-[#C850C0] to-[#FFCC70] rounded-full w-[120px] animate-bounce my-10" />
 
-        <button className="text-white font-normal text-[23px] leading-[30px] bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70] w-[150px] h-[53px] rounded-2xl mt-6">Sair</button>
+        <button
+          onClick={ (): void => navigate("/") }
+          className="text-white font-normal text-[23px] leading-[30px] bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70] w-[150px] h-[53px] rounded-2xl mt-6"
+        >
+          Sair
+        </button>
       </div>
     </section>
   );
