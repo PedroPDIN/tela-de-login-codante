9cbdada06e28f3f1960ebc63c84fe496679bfa07
Pedro Lima <pedro.oficial1969@gmail.com>
2023-06-07 15:33:32 -0300

    style(resposive): adiciona estilização resposiva a mas (lg-laptops) para página de welcome.

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -5,7 +5,7 @@ function Welcome() {
   const navigate = useNavigate();
 
   return (
-    <section className="bg-[#070709] rounded-[34px] h-[480px] w-[800px] max-sm:w-[440px] sm:max-md:w-[470px]">
+    <section className="bg-[#070709] rounded-[34px] h-[480px] w-[800px] max-sm:w-[440px] sm:max-md:w-[470px] md:max-lg:w-[700px]">
       <div className="w-full h-full flex flex-col items-center justify-center">
         <h1 className="text-white font-normal text-[60px] text-center flex items-center justify-center w-full max-sm:text-[48px] sm:max-md:text-[50px]">
           Seja bem vindo<p className="text-transparent bg-clip-text bg-gradient-to-b from-[#4158D0] via-[#C850C0] to-[#FFCC70] font-normal text-[70px] max-sm:text-[48px] sm:max-md:[50px]">
@@ -13,7 +13,7 @@ function Welcome() {
           </p>
         </h1>
 
-        <BsFillCheckCircleFill className="text-[#070709] text-[100px] bg-gradient-to-t from-[#4158D0] via-[#C850C0] to-[#FFCC70] rounded-full w-[120px] animate-bounce my-10 max-sm:text-[70px] max-sm:w-[90px] sm:max-md:text-[80px] sm:max-md:w-[100px]" />
+        <BsFillCheckCircleFill className="text-[#070709] text-[100px] bg-gradient-to-t from-[#4158D0] via-[#C850C0] to-[#FFCC70] rounded-full w-[120px] animate-bounce my-10 max-sm:text-[70px] max-sm:w-[90px] sm:max-md:text-[80px] sm:max-md:w-[100px] md:max-lg:text-[90px] md:max-lg:w-[110px]" />
 
         <button
           onClick={ (): void => navigate("/") }
